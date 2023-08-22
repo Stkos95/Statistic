d0a7487e396c3904b2525091ca52981a30d3aee2
Stkos95 <plstkos@yandex.ru>
2023-08-22 13:46:54 +0300

    Fixed URLS data and categories, add filter cat+date

--- tasks/urls.py
+++ tasks/urls.py
@@ -6,10 +6,9 @@ from . import views
 
 urlpatterns = [
     path('schedule/<int:task_id>/', views.task_detail, name='task_detail'),
-    path('schedule/', views.schedule, name='schedule'),
-    path('schedule/category/<slug:category_slug>/', views.schedule, name='schedule_category'),
-    path('schedule/<date>/<slug:category_slug>/', views.schedule, name='schedule_category_time'),
-    path('schedule/<date>/', views.schedule, name='schedule_time'),
+    path('schedule/', views.schedule_redirect, name='schedule'),
+    path('schedule/<slug:category_slug>/', views.schedule, name='schedule_category'),
+    path('schedule/<slug:category_slug>/<date>/', views.schedule, name='schedule_time'),
     path('about/', views.about, name='about'),
 
 
--- tasks/views.py
+++ tasks/views.py
@@ -1,28 +1,30 @@
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Task, Category
 
 menu = [{'name': "Schedule", 'link': '#'},
         {'name': 'About', 'link': '#'}]
 
 
-def schedule(request, date=None, category_slug=None):
+def schedule_redirect(request):
+    return redirect('schedule_category', category_slug='all')
+
+
+def schedule(request, category_slug, date=None):
     all_categories = Category.objects.all()
     all_tasks = Task.objects.all()
     if category_slug == 'empty':
         all_tasks = all_tasks.filter(category__isnull=True)
 
-    elif category_slug:
+    elif category_slug != 'empty' and category_slug != 'all':
+
         all_tasks = all_tasks.filter(category__slug=category_slug)
 
     if date:
         d = '-'.join(date.split('-')[::-1])
         all_tasks = all_tasks.filter(deadline__date=d)
 
-
-
     return render(request, 'tasks/schedule.html', {'tasks': all_tasks,
-                                                   'menu': menu,
                                                    'section': 'schedule',
                                                    'all_categories': all_categories,
                                                    'category_slug': category_slug})
