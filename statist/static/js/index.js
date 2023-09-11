758d27f84b6c37855bcd6e36e545643451168001
Stkos95 <plstkos@yandex.ru>
2023-09-11 16:23:14 +0300

    fix add player logic, now it copy the whole hidden player-form

--- statist/static/js/index.js
+++ statist/static/js/index.js
@@ -1,102 +1,57 @@
 
-function addPlayer(element){
-
+function addPlayer(element=null){
     let form = document.querySelector('form');
     let confirmButton = document.querySelector('form input[type="submit"]');
 
-    // if (element.previousElementSibling.value.trim() === '') {
-    // return false;
-    // }
-    let maxNumbers = []
-    let maxValue = document.querySelectorAll('input.players');
-    maxValue.forEach((e) => {
+    let divCopied = document.querySelector('div.player-form').cloneNode(true)
 
-        maxNumbers.push(parseInt(e.getAttribute('name')))
-    })
+    divCopied.style.display = 'block'
+    form.insertBefore(divCopied, confirmButton)
 
-    let max = Math.max(...maxNumbers) + 1
-
-
-
-
-    // let div = document.querySelector('div.player-form')
-    let divCopied = document.querySelector('div.fields').cloneNode(true)
-    console.log(divCopied)
-
-
-
-    let div = document.createElement('div')
-    div.setAttribute('class', 'player-form')
-
-    form.insertBefore(div, confirmButton)
-
-
-
-    // div.insertAdjacentHTML('afterbegin', '<span class="test">')
-
-
-    // let field = document.createElement('input')
-    // field.setAttribute('type', 'text')
-    // field.setAttribute('name', max)
-    // field.classList.add('players')
-
-    let plus = document.createElement('span');
-    plus.setAttribute('onclick', 'addPlayer(this)')
-    plus.classList.add('plus-button')
-    let plusText = document.createTextNode('+');
-    plus.appendChild(plusText)
-
-    let minus = document.createElement('span');
-    minus.setAttribute('onclick', 'removePlayer(this)')
-    minus.classList.add('minus-button')
-    let minusText = document.createTextNode('-')
-    minus.appendChild(minusText)
-
-    div.appendChild(divCopied)
-    div.appendChild(plus)
-    div.appendChild(minus)
+    let minus = divCopied.childNodes[5]
 
     let players = document.querySelectorAll('.player-form')
-    if (players.length > 1) {
+    if (players.length > 2) {
         minus.style.display = 'inline'
 
-
     }
-
-
-    element.nextElementSibling.style.display = 'inline'
-
-    element.style.display = 'none'
-
-
+    if (element) {
+        element.nextElementSibling.style.display = 'inline'
+        element.style.display = 'none'
+    }
+    const inputFields = divCopied.querySelectorAll('.count-field')
+    for (let el of inputFields) {
+        createPlusMinusButtons(el)
+    }
 }
 
 
 
 
 function removePlayer(element) {
+    let conf
+    let flag = false
+    const parentDiv = element.previousElementSibling.previousElementSibling
+    for (let field of parentDiv.children) {
+        if (field.tagName === 'INPUT' && field.value && !flag){
+            conf = confirm('В строке введено значение, удалить?')
+            flag = true
+            break
 
-
-    if(element.previousElementSibling.previousElementSibling.value) {
-        let conf = confirm('В строке введено значение, удалить?')
-
-        if (conf) {
-            element.parentNode.remove()
         }
-
     }
-    else {
-            element.parentNode.remove()
-        }
 
-    let buttons = document.querySelectorAll('.plus-button')
+    if (conf || !flag) {
+        element.parentNode.remove()
+    }
+
+    let buttons = document.querySelectorAll('.add-player')
      let LastButton = buttons[buttons.length - 1];
-    if (buttons.length === 1) {
-
-        LastButton.previousElementSibling.style.display = 'inline'
+    if (buttons.length === 2) {
         LastButton.nextElementSibling.style.display = 'none'
     }
-
     LastButton.style.display = 'inline'
 
 }
+
+
