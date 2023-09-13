a703e6c9d6cc1ca1e7c028806cb046a1c29b8e1e
Stkos95 <plstkos@yandex.ru>
2023-09-13 16:42:14 +0300

    add checkbox and click event listener, finish table, add data to db

--- statist/static/js/index.js
+++ statist/static/js/index.js
@@ -1,16 +1,20 @@
-
-function addPlayer(element=null){
-    let form = document.querySelector('form');
+function addPlayer(element = null) {
+    let form = document.forms[0];
+    let table = document.querySelector('table')
     let confirmButton = document.querySelector('form input[type="submit"]');
 
-    let divCopied = document.querySelector('div.player-form').cloneNode(true)
 
-    divCopied.style.display = 'block'
-    form.insertBefore(divCopied, confirmButton)
+    let rowCopied = document.querySelector('.field-row').cloneNode(true)
 
-    let minus = divCopied.childNodes[5]
+    rowCopied.style.display = 'table-row'
+    let c = table.insertRow(-1)
+    c.classList.add('field-row')
 
-    let players = document.querySelectorAll('.player-form')
+    c.innerHTML = rowCopied.innerHTML
+
+    let minus = c.querySelector('.remove-player')
+
+    let players = document.querySelectorAll('.field-row')
     if (players.length > 2) {
         minus.style.display = 'inline'
 
@@ -19,39 +23,71 @@ function addPlayer(element=null){
         element.nextElementSibling.style.display = 'inline'
         element.style.display = 'none'
     }
-    const inputFields = divCopied.querySelectorAll('.count-field')
-    for (let el of inputFields) {
-        createPlusMinusButtons(el)
-    }
+    // const inputFields = divCopied.querySelectorAll('.count-field')
+    // for (let el of inputFields) {
+    //     createPlusMinusButtons(el)
+    // }
+
 }
 
-
+//
+// function removePlayer(element) {
+//     let conf
+//     let flag = false
+//     const parentDiv = element.parentNode.parentNode
+//     // console.log(parentDiv.children[0].childNodes[0])
+//     parentDiv.remove()
+//     //
+//     for (let field of parentDiv.children) {
+//         console.log(field.childNodes[0])
+//
+//
+//     }
+//         // if (field.childNodes[0].tagName === 'INPUT' && field.value && !flag){
+//     //         conf = confirm('В строке введено значение, удалить?')
+//     //         flag = true
+//     //         break
+//     //
+//     //     }
+//     // }
+//     //
+//     // if (conf || !flag) {
+//     //     element.parentNode.remove()
+//     // }
+//     //
+let buttons = document.querySelectorAll('.add-player')
+let LastButton = buttons[buttons.length - 1];
+if (buttons.length === 2) {
+    LastButton.nextElementSibling.style.display = 'none'
+}
+LastButton.style.display = 'inline'
+//
+// }
 
 
 function removePlayer(element) {
-    let conf
-    let flag = false
-    const parentDiv = element.previousElementSibling.previousElementSibling
-    for (let field of parentDiv.children) {
-        if (field.tagName === 'INPUT' && field.value && !flag){
-            conf = confirm('В строке введено значение, удалить?')
-            flag = true
-            break
+    let conf = true
+    const parentElement = element.parentNode.parentNode
 
+    for (let field of parentElement.children) {
+        if (field.children[0].tagName === 'INPUT' && field.children[0].value) {
+            conf = confirm('В строке введено значение, удалить?')
+            break
         }
+
     }
 
-    if (conf || !flag) {
-        element.parentNode.remove()
+
+    if (conf) {
+        parentElement.remove()
     }
 
     let buttons = document.querySelectorAll('.add-player')
-     let LastButton = buttons[buttons.length - 1];
+    let LastButton = buttons[buttons.length - 1];
     if (buttons.length === 2) {
         LastButton.nextElementSibling.style.display = 'none'
     }
     LastButton.style.display = 'inline'
 
+
 }
-
-
